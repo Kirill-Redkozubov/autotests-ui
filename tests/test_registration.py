1956082f2a950a92e0fd4f4bb603c387eb5f103e
Kirill Redkozubov <kirillred1996@mail.ru>
2026-05-12 20:49:55 -0700

    Знакомствос PageObject. Практика работы с PageObject

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,27 +1,35 @@
 from playwright.sync_api import expect, Page
 import pytest
+from pages.dashboard_page import DashboardPage
+from pages.registration_page import RegistrationPage
+
 
 @pytest.mark.regression
 @pytest.mark.registration
-def test_successful_registration(chromium_page: Page):  # Создаем тестовую функцию
-    # Все остальные действия остаются без изменений
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage): 
+    registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    registration_page.fill_registration_form(email="user@gmail.com", username="username", password="1234")
+    registration_page.click_registration_button()
+    
+    dashboard_page.check_visible_dashboard()
 
-    chromium_page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
-    email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.fill('user@gmail.com')
+    # chromium_page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
-    username_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
-    username_input.fill('username')
+    # email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
+    # email_input.fill('user@gmail.com')
 
-    password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
-    password_input.fill('password')
+    # username_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
+    # username_input.fill('username')
 
-    registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
-    registration_button.click()
+    # password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
+    # password_input.fill('password')
 
-    dashboard_title = chromium_page.get_by_test_id("dashboard-toolbar-title-text")
-    expect(dashboard_title).to_be_visible()
+    # registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
+    # registration_button.click()
+
+    # dashboard_title = chromium_page.get_by_test_id("dashboard-toolbar-title-text")
+    # expect(dashboard_title).to_be_visible()
 
     # with sync_playwright() as playwright:
     #     browser = playwright.chromium.launch(headless=False)
